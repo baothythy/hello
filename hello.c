4a9a682a769880e9a3f6ddcfd217c9df5aece155
Thy Nguyen <tnguyen169@huskers.unl.edu>
2021-08-29 08:45:12 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ * Author: Thy Nguyen
+ *         tnguyen169@huskers.unl.edu
+ * Date: 2021/08/29
+ *
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Thy Nguyen\n");
+
+  return 0;
+}
